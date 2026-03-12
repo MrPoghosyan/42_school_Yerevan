6c8fc1b73782b3860a0c9edbed6714cd9d749315
MrPoghosyan <installation.v.p@gmail.com>
2026-03-12 22:51:47 +0400

    Improve zombieHorde: validate N and generate unique zombie names Return NULL when N <= 0 to avoid invalid allocation. Generate unique names for each zombie using an index suffix.

--- main_projects/C++_Module_01/ex01/zombieHorde.cpp
+++ main_projects/C++_Module_01/ex01/zombieHorde.cpp
@@ -1,11 +1,18 @@
+#include <sstream>
 #include "Zombie.hpp"
 
 Zombie* zombieHorde( int N, std::string name )
 {
+	if (N <= 0)
+		return NULL;
+
 	Zombie* horde = new Zombie[N];
 
-	for (int i = 0; i < N; i++)
-		horde[i].setName(name);
+	for (int i = 0; i < N; i++){
+		std::ostringstream ss;
+		ss << name << i+1;
+		horde[i].setName(ss.str());
+	}
 
 	return horde;
 }
