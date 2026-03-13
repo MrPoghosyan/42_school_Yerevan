9426132c76ff387f00423c070ba9f2b7dc207a0f
MrPoghosyan <installation.v.p@gmail.com>
2026-03-13 23:58:48 +0400

    Add ex03

---
+++ main_projects/C++_Module_01/ex03/Weapon.cpp
@@ -0,0 +1,15 @@
+#include "Weapon.hpp"
+
+Weapon::Weapon(std::string type)
+	:type(type)
+{}
+
+void Weapon::setType(const std::string& type)
+{
+	this->type = type;
+}
+
+const std::string& Weapon::getType() const
+{
+	return this->type;
+}
