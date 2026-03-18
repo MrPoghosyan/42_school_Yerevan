3ba2b39245def3cf8eb25b51109affb185fced50
MrPoghosyan <installation.v.p@gmail.com>
2026-03-18 21:24:36 +0400

    Add ex00 file

---
+++ main_projects/C++_Module_02/ex00/Fixed.cpp
@@ -0,0 +1,40 @@
+#include "Fixed.hpp"
+#include <iostream>
+
+Fixed::Fixed()
+	:_value(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed( const Fixed& other )
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+int Fixed::getRawBits( void ) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return _value;
+}
+
+Fixed& Fixed::operator=(const Fixed& other)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+
+	if (this != &other)
+		this->_value = other.getRawBits();
+
+	return *this;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+void Fixed::setRawBits( int const raw )
+{
+	_value = raw;
+}
---
+++ main_projects/C++_Module_02/ex00/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Fixed.hpp"
+
+int main()
+{
+	Fixed a;
+//	a.setRawBits(42);
+
+	Fixed b(a);
+	Fixed c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return 0;
+}
